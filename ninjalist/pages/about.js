1811c3bd63b13656a00df6f9a674ba0e7c78e22c
GoldenPaladin <yacine.ezdini@gmail.com>
2021-12-19 08:48:39 +0100

    Pages & Routes

---
+++ ninjalist/pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, voluptates
+        impedit? Assumenda, labore aperiam. Tenetur laudantium saepe quaerat
+        modi voluptate incidunt rerum cupiditate possimus quam atque impedit
+        delectus, dolor harum.
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. At soluta
+        velit odit! Perspiciatis optio tempore numquam soluta cum hic magnam
+        molestiae quia, nesciunt illo consequuntur in, deleniti repudiandae
+        impedit? Nam?
+      </p>
+    </div>
+  );
+};
+
+export default About;
