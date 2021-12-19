774d052bd87830f4415ee649acec14404fe73049
GoldenPaladin <yacine.ezdini@gmail.com>
2021-12-19 19:24:32 +0100

    Layout + CSS + 404

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -8,23 +8,21 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <h1>HomePage</h1>
-      <p>
+      <h1 className={styles.title}>HomePage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem
         optio cum maiores asperiores. Harum commodi voluptas magni non neque ab
         placeat tenetur nam reiciendis. Similique dolorem unde neque doloribus?
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
         libero eius natus id iure dignissimos tempora laboriosam pariatur
         consectetur, similique eligendi nisi explicabo quos ab architecto
         consequatur a doloremque dolorum!
       </p>
       <Link href="/ninjas">
-        <a> See Ninja Listing</a>
+        <a className={styles.btn}> See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
