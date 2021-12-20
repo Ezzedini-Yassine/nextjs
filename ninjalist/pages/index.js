b92e4de5d1c42e1940df952c2e8215d0c118b1ff
GoldenPaladin <yacine.ezdini@gmail.com>
2021-12-20 08:16:24 +0100

    Images & Metadata

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -7,22 +7,29 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>HomePage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem
-        optio cum maiores asperiores. Harum commodi voluptas magni non neque ab
-        placeat tenetur nam reiciendis. Similique dolorem unde neque doloribus?
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-        libero eius natus id iure dignissimos tempora laboriosam pariatur
-        consectetur, similique eligendi nisi explicabo quos ab architecto
-        consequatur a doloremque dolorum!
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}> See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>HomePage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorem
+          optio cum maiores asperiores. Harum commodi voluptas magni non neque
+          ab placeat tenetur nam reiciendis. Similique dolorem unde neque
+          doloribus?
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
+          libero eius natus id iure dignissimos tempora laboriosam pariatur
+          consectetur, similique eligendi nisi explicabo quos ab architecto
+          consequatur a doloremque dolorum!
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}> See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
